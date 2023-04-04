3e727ce4954c1c42e1db6d329e21d97d0f53f339
DPU09 <DL-OTTCloudPlatform-Nova@bskyb.internal>
2023-04-04 23:29:14 +0100

    edit README

--- src/test/java/com/kata/katawithdom/AdapterTest.java
+++ src/test/java/com/kata/katawithdom/AdapterTest.java
@@ -1,8 +1,12 @@
 package com.kata.katawithdom;
 
 import com.kata.katawithdom.adapter.*;
+import com.kata.katawithdom.factory.Square;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AdapterTest {
 
